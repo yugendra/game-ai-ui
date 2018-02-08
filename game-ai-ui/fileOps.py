d72130be961a4b636c6a420fdc9870a8d948af5e
yugendra <yugendra.mail@gmail.com>
2018-02-08 19:51:31 +0530

    updated script location

--- game-ai-ui/fileOps.py
+++ game-ai-ui/fileOps.py
@@ -1,21 +1,23 @@
 import subprocess
 import os
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 def readFile(user):
     lines = ''
-    with open(os.getcwd() + '/files/' + user + '.py') as fp:
+    with open(script_dir + '/files/' + user + '.py') as fp:
         for line in fp:
             lines = lines + line
     return lines
     
 def writeFile(user, data):
-    f = open(os.getcwd() + '/files/' + user + '.py','w')
+    f = open(script_dir + '/files/' + user + '.py','w')
     f.write(data)
     
 def runFile(user):
-    result = subprocess.Popen(['python', os.getcwd() + '/files/' + user + '.py'], stdout=subprocess.PIPE)
+    result = subprocess.Popen(['python', script_dir + '/files/' + user + '.py'], stdout=subprocess.PIPE)
     stdout = result.communicate()[0]
     return stdout
     
 def createUserEnv(user):
-    subprocess.Popen(['cp', '-f', os.getcwd() + '/files/code.py', os.getcwd() + '/files/' + user + '.py'], stdout=subprocess.PIPE)
+    subprocess.Popen(['cp', '-f', script_dir + '/files/code.py', script_dir + '/files/' + user + '.py'], stdout=subprocess.PIPE)
